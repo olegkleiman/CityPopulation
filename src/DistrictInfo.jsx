d5dbec53f685c0774192d375a5b017de5ec65ba9
Oleg Kleiman <oleg_kleyman@hotmail.com>
2019-03-30 20:57:17 +0300

    .

--- src/DistrictInfo.jsx
+++ src/DistrictInfo.jsx
@@ -12,25 +12,21 @@ const DistrictInfo = ({info}) => {
 
   const [hintValue, setHintValue] = useState({});
 
-  const data = [
-        // {x: '2009', y: 750.5},
-        {x: '2010', y: 734.3},
-        {x: '2011', y: 505.2},
-        {x: '2012', y: 75.3},
-        {x: '2013', y: 754.2},
-        {x: '2014', y: 603.7},
-        {x: '2015', y: 804.3},
-        {x: '2016', y: 905.9}
-      ];
+  const data = [];
 
   const districtId = info.districtInfo.id;
-  for(const year in SheetData ){
-    console.log(year);
-    const value = SheetData[year].map.get(districtId);
-    data.push( {
-      x: year.toString(),
-      y: value.total
-    })
+  for(const year in SheetData ){ // enum props only
+
+    if( SheetData.hasOwnProperty(year) ) {
+
+      if( !isNaN(parseInt(year, 10)) ) {
+        const value = SheetData[year].map.get(districtId);
+        data.push( {
+          x: year.toString(),
+          y: value.total
+        })
+      }
+    }
   };
 
 
--- src/SheetData.js
+++ src/SheetData.js
@@ -1,3 +1,38 @@
-let sheetData = {}
+let sheetData = {
+
+  load: function (sheetId) {
+    console.log('load started');
+
+    ['2010', '2011', '2012', '2013', '2014', '2015', '2016'].map( async(sheetName) => {
+        const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=AIzaSyABrJkY9bVKLn3YB8f4kmiiGBDWhv4goYA&majorDimension=ROWS`;
+        const resp = await fetch(url);
+        const respJson = await resp.json();
+
+        const districtsData = respJson.values;
+
+        let columns = [];
+        const districtsMap = new Map();
+        districtsData.forEach( (item, index) => {
+          if( index === 0 ) {
+            columns = item;
+          } else {
+            const columnIndex = columns.findIndex( item => item === 'total' );
+            districtsMap.set(item[0], {
+              "total": parseFloat(item[columnIndex].replace(/,/g, '')),
+            });
+          }
+        });
+
+        this[sheetName] = {
+          map: districtsMap,
+          rawData: districtsData
+        }
+    })
+
+  }
+}
+
+const sheetId = '1GJWgz04VTdJIPT5y4N8qkY-fkNc3lvbxATv8TeC-deA';
+sheetData.load(sheetId);
 
 export default sheetData;
